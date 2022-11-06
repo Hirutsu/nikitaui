ae521b65dcb169292efd9d0c209bcb0784d2c45c
Levan Burchuladze <levan.burchuladze@rokolabs.com>
2022-11-06 18:59:22 +0400

    ff

--- src/App.js
+++ src/App.js
@@ -3,13 +3,80 @@ import {BrowserRouter} from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
+import Fullpage,{FullPageSections, FullpageSection, FullpageNavigation} from '@ap.cx/react-fullpage'
+import main_1 from './main_1.png'
+import main_2 from './main_2.png'
+import main_3 from './main_3.png'
+import main_4 from './main_4.png'
+import main_5 from './main_5.png'
+import main_6 from './main_6.png'
+import main_7 from './main_7.png'
+import main_8 from './main_8.png'
+import main_9 from './main_9.png'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Content />
-      <Footer />
+      <Fullpage>
+        <FullpageNavigation/>
+        <FullPageSections>
+          
+          <FullpageSection style={{backgroundImage: `url(${main_1})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_2})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_3})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_4})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_5})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_6})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_7})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_8})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+          <FullpageSection style={{backgroundImage: `url(${main_9})`, backgroundSize: 'cover'}}>
+            <Header />
+            <Content />
+            <Footer />
+          </FullpageSection>
+
+        </FullPageSections>
+      </Fullpage>
     </BrowserRouter>
   );
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,31 +1,23 @@
-import Fullpage,{FullPageSections, FullpageSection, FullpageNavigation} from '@ap.cx/react-fullpage'
-
 function Main() {
     return (
-        <Fullpage>
-            <FullPageSections>
-                <FullpageSection>
-                    <div className='main'>
-                        <div className='main_info'>
-                        <div className='main_info_header'>
-                        <div className='main_info_header_text'>
-                            Онкологическая служба<br/>Саратовской области</div>
-                        </div>
-                        <div className='redline'>
-                            <img className='red_line'/>
-                        </div>
-                        <div className='main_info_text'>
-                        История онкологической службы Саратовской области начинается с создания первого Областного онкологического диспансера, организованного 
-                        на территории г. Энгельса в сентябре 1945 года по распоряжению Совета Народных Комиссаров СССР специальным постановлением «О мероприятиях 
-                        по улучшению онкологической помощи населению».С 8 февраля 1946 года диспансер стал выполнять функции Саратовского областного онкологического 
-                        диспансера.
-                        </div>
-                        <button className='more'>Подробнее</button>
-                        </div>
-                    </div>
-                </FullpageSection>
-            </FullPageSections>
-        </Fullpage>
+        <div className='main'>
+            <div className='main_info'>
+            <div className='main_info_header'>
+            <div className='main_info_header_text'>
+                Онкологическая служба<br/>Саратовской области</div>
+            </div>
+            <div className='redline'>
+                <img className='red_line'/>
+            </div>
+            <div className='main_info_text'>
+            История онкологической службы Саратовской области начинается с создания первого Областного онкологического диспансера, организованного 
+            на территории г. Энгельса в сентябре 1945 года по распоряжению Совета Народных Комиссаров СССР специальным постановлением «О мероприятиях 
+            по улучшению онкологической помощи населению».С 8 февраля 1946 года диспансер стал выполнять функции Саратовского областного онкологического 
+            диспансера.
+            </div>
+            <button className='more'>Подробнее</button>
+            </div>
+        </div>
     );
   }
   
